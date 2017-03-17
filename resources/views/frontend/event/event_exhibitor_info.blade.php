831a69e016cda88ef8656db793a28258562e38f3
Aung Ko Khant <aungkokhantace@gmail.com>
2017-03-17 10:14:16 +0630

    hide indicator in sliders

--- resources/views/frontend/event/event_exhibitor_info.blade.php
+++ resources/views/frontend/event/event_exhibitor_info.blade.php
@@ -109,13 +109,13 @@
         <!-- Carousel Slider Part -->
         <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
             <!-- Indicators -->
-            <ol class="carousel-indicators">
-                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="3"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="4"></li>
-            </ol>
+            {{--<ol class="carousel-indicators">--}}
+                {{--<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>--}}
+                {{--<li data-target="#carousel-example-generic" data-slide-to="1"></li>--}}
+                {{--<li data-target="#carousel-example-generic" data-slide-to="2"></li>--}}
+                {{--<li data-target="#carousel-example-generic" data-slide-to="3"></li>--}}
+                {{--<li data-target="#carousel-example-generic" data-slide-to="4"></li>--}}
+            {{--</ol>--}}
             <!-- Wrapper for slides -->
             <div class="carousel-inner" role="listbox">
                 @if(isset($images) && count($images)>0)
@@ -131,14 +131,14 @@
                 @endif
             </div>
             <!-- Controls -->
-            {{--<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">--}}
-            {{--<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>--}}
-            {{--<span class="sr-only">Previous</span>--}}
-            {{--</a>--}}
-            {{--<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">--}}
-            {{--<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>--}}
-            {{--<span class="sr-only">Next</span>--}}
-            {{--</a>--}}
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+            </a>
         </div>
         <!-- end of Carousel & header section -->
         <br>
